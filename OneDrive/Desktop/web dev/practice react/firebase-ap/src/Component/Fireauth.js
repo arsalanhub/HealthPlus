56108237178ce8bc8aafbebbe52ce142e85b41e1
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-03-27 17:57:22 +0530

    dashboard added

--- OneDrive/Desktop/web dev/practice react/firebase-ap/src/Component/Fireauth.js
+++ OneDrive/Desktop/web dev/practice react/firebase-ap/src/Component/Fireauth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { auth } from "../Firebase";
 
 export default function Fireauth() {
@@ -10,21 +10,36 @@ export default function Fireauth() {
     let res = await auth.createUserWithEmailAndPassword(email, password);
     console.log(res);
   };
+
+  useEffect(() => {
+    let unsub = auth.onAuthStateChanged((user) => {
+      setUser(user);
+      return () => {
+        unsub(); // cleanup
+      };
+    });
+  }, []);
   return (
     <>
-      <label>Email</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label>Password</label>
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={create}>Click Me</button>
+      {user == null ? (
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={create}>Click Me</button>
+        </div>
+      ) : (
+        <div>{user.email}</div>
+      )}
     </>
   );
 }
